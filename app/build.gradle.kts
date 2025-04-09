b738c8d34123621793420ca39e0c9e2b1e44da3e
Okkoman <okkoman0xx0@gmail.com>
2025-04-09 19:41:18 +0200

    Workflow : add android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,11 @@
 plugins {
     id("com.android.application")
-    // Add the Google services gradle plugin
-    //id("com.google.gms.google-services")
     kotlin("android")
-    kotlin("android.extensions")
 }
 
 val kotlinVersion: String by ext
 val ndkSideBySideVersion: String by ext
 val cmakeVersion: String by ext
-val buildStagingDir: String by ext
-
-val keystorePropertiesFile = rootProject.file("keystore.properties")
 
 fun loadKeystoreProperties(file: File): Map<String, String> {
     return file.reader().use { reader ->
@@ -25,115 +19,123 @@ fun loadKeystoreProperties(file: File): Map<String, String> {
     }
 }
 
-val keystoreProperties = loadKeystoreProperties(keystorePropertiesFile)
-
 android {
+    namespace = "com.okkomastudio.galaxianmatch"
+    compileSdk = 34
     ndkVersion = ndkSideBySideVersion
-    ndkPath = "/media/DATA/Dev/android-sdk/ndk/$ndkVersion"
-    compileSdkVersion(31)
 
     defaultConfig {
-        minSdk = 19
+        applicationId = "com.okkomastudio.galaxianmatch"
+        minSdk = 21
         targetSdk = 34
         versionCode = 34
         versionName = "1.034"
         multiDexEnabled = true
 
-//        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
-                arguments.apply {
-                    System.getenv("ANDROID_CCACHE")?.let { add("-D ANDROID_CCACHE=$it") }
-                    arguments += listOf("-D URHO3D_LIB_TYPE=STATIC", "-D URHO3D_ANGELSCRIPT=0", "-D URHO3D_LUA=0", "-D URHO3D_LUAJIT=0")
-                    arguments += listOf("-D URHO3D_NETWORK=0")
-                    arguments += listOf("-D SPACEMATCH_WITH_DEMOMODE=0", "-D SPACEMATCH_WITH_TIPS=0", "-D SPACEMATCH_WITH_ADS=1", 
-                                        "-D SPACEMATCH_WITH_CINEMATICS=1", "-D SPACEMATCH_WITH_LOOPTESTS=0" , "-D SPACEMATCH_WITH_NETWORK=0")                               
+                arguments += listOf(
+                    "-DURHO3D_LIB_TYPE=STATIC",
+                    "-DURHO3D_ANGELSCRIPT=0",
+                    "-DURHO3D_LUA=0",
+                    "-DURHO3D_LUAJIT=0",
+                    "-DURHO3D_NETWORK=0",
+                    "-DSPACEMATCH_WITH_DEMOMODE=0",
+                    "-DSPACEMATCH_WITH_TIPS=0",
+                    "-DSPACEMATCH_WITH_ADS=1",
+                    "-DSPACEMATCH_WITH_CINEMATICS=1",
+                    "-DSPACEMATCH_WITH_LOOPTESTS=0",
+                    "-DSPACEMATCH_WITH_NETWORK=0"
+                )
+                System.getenv("ANDROID_CCACHE")?.let {
+                    arguments += "-DANDROID_CCACHE=$it"
                 }
-
-                // Sets a flag to enable format macro constants for the C compiler.
                 cFlags += listOf("-std=c99")
                 cppFlags += listOf("-std=c++98")
-
                 targets.add("GalaxianMatch")
             }
         }
     }
 
 	signingConfigs {
-    	create("release") {
-      		keyAlias = keystoreProperties["keyAlias"]
-            keyPassword = keystoreProperties["keyPassword"]
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"]
-    	}
- 	}
- 	
+		create("release") {
+			val keystoreFilePath = System.getenv("KEYSTORE_FILE") ?: ""
+			if (keystoreFilePath.isEmpty()) {
+				val keystoreProperties = loadKeystoreProperties(rootProject.file("keystore.properties"))
+				keyAlias = keystoreProperties["keyAlias"]
+				keyPassword = keystoreProperties["keyPassword"]
+				storeFile = file(keystoreProperties["storeFile"] as String)
+				storePassword = keystoreProperties["storePassword"]
+			}
+			else {
+				keyAlias = System.getenv("KEY_ALIAS")
+				keyPassword = System.getenv("KEY_PASSWORD")
+				storeFile = file(keystoreFilePath)
+				storePassword = System.getenv("KEYSTORE_PASSWORD")
+			}
+		}
+	}
+
+    buildTypes {
+        getByName("debug") {
+            isJniDebuggable = true
+            isMinifyEnabled = false
+        }
+        getByName("release") {
+            isJniDebuggable = false
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
     flavorDimensions += "graphics"
     productFlavors {
         create("gl") {
             dimension = "graphics"
             applicationIdSuffix = ".gl"
             versionNameSuffix = "-gl"
+
             externalNativeBuild {
                 cmake {
-                    arguments += listOf("-D URHO3D_OPENGL=1")
+                    arguments += "-DURHO3D_OPENGL=1"
                 }
             }
+
             splits {
                 abi {
-                    isEnable = true//project.hasProperty("ANDROID_ABI")
+                    isEnable = true
                     reset()
                     include("armeabi-v7a", "arm64-v8a")
-                    //include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
                 }
             }
         }
-    }
-
-    buildTypes {
-        named("debug") {
-            isJniDebuggable = true
-            isMinifyEnabled = false
-        }
-        named("release") {
-            isJniDebuggable = false
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-
-    lintOptions {
-        isAbortOnError = false
     }
 
     externalNativeBuild {
         cmake {
             version = cmakeVersion
-            path = project.file("../CMakeLists.txt")
-            buildStagingDirectory(buildStagingDir)
+            path = file("../CMakeLists.txt")
+            buildStagingDirectory = file(".cxx")
         }
     }
 
-    sourceSets {
-        named("main") {
-            assets.srcDir(project.file("../bin"))
-        }
+    sourceSets["main"].assets.srcDir(file("../bin"))
+
+    lint {
+        abortOnError = false
     }
 }
 
-// We build urho3d as submodule:
-// We don't need to link anymore to a prebuild library 
-
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    implementation(fileTree("libs") { include("*.jar", "*.aar") })
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-// MultiDex Dependency
+	// MultiDex Dependency
     implementation("androidx.multidex:multidex:2.0.1")
-// Google Services Dependencies
+	// Google Services Dependencies
     implementation("com.google.android.gms:play-services-ads:22.2.0")
     implementation("com.android.billingclient:billing:6.0.1")
 // Import the Firebase BoM
@@ -148,10 +150,8 @@ dependencies {
 //    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
 
-
-tasks {
-    register<Delete>("cleanAll") {
-        dependsOn("clean")
-        delete = setOf(android.externalNativeBuild.cmake.buildStagingDirectory)
-    }
+tasks.register<Delete>("cleanAll") {
+    dependsOn("clean")
+    delete(android.externalNativeBuild.cmake.buildStagingDirectory)
 }
+
